732c6d37264c903f667d0f008782d287dc1f271c
snouel <104464911+snouel@users.noreply.github.com>
2024-11-19 19:26:04 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'firstApp';
+  images = [
+    'https://www.newegg.com/insider/wp-content/uploads/windows_xp_bliss-wide.jpg',
+    'https://wparena.com/wp-content/uploads/2009/09/img0.jpg',
+    'https://www.betaarchive.com/imageupload/2012-06/1340169550.or.38053.png',
+    'https://c4.wallpaperflare.com/wallpaper/845/501/105/windows-vista-aero-45-wallpaper-preview.jpg',
+    'https://ia801909.us.archive.org/30/items/win10-backgrounds/Win10%20DBs/img0_3840x2160.jpg',
+    'https://wallpaper.forfun.com/fetch/70/70e83ab44b08fa76ad376182252d771f.jpeg',
+    'https://www.newegg.com/insider/wp-content/uploads/windows_xp_bliss-wide.jpg',
+    'https://wparena.com/wp-content/uploads/2009/09/img0.jpg',
+    'https://www.betaarchive.com/imageupload/2012-06/1340169550.or.38053.png',
+    'https://c4.wallpaperflare.com/wallpaper/845/501/105/windows-vista-aero-45-wallpaper-preview.jpg',
+    'https://ia801909.us.archive.org/30/items/win10-backgrounds/Win10%20DBs/img0_3840x2160.jpg',
+    'https://wallpaper.forfun.com/fetch/70/70e83ab44b08fa76ad376182252d771f.jpeg'
+
+  ];
+
+
 }
